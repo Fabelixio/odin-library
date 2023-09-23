b21c620446ebe0cd77821dc4ffa080fbceb98945
Fabelixio <felixsewellknight@gmail.com>
2023-09-23 17:01:18 +0100

    add book form js functionality

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-/* Write constructor to build book objects. Function takes user input, puts it into an
-array. Function loops through array and displays each book on display card.
+/*  Function loops through array and displays each book on display card.
 New book button allows user to input details that are fed into constructor.
-Add button to remove each book, another to edit each book.*/
+ another to edit each book.*/
 
 const library = [];
 const bookContainer = document.querySelector('.bookContainer')
@@ -29,6 +28,7 @@ function addBook(newBook) {
 // adds and defines read status toggle and book card delete buttons to book card
 
 function libraryDisplay() {
+    clearDisplay()
     library.forEach((item, index) => {
         book = document.createElement('div')
         book.classList.add('bookCard')
@@ -66,15 +66,53 @@ function libraryDisplay() {
     })
 }
 
+// refreshes display to avoid multiple library instances
 
+function clearDisplay() {
+    while (bookContainer.lastChild) {
+        bookContainer.removeChild(bookContainer.lastChild)
+    }
+}
 
+// removes book card from library
 
+function removeBook(index) {
+    library.splice(index, 1)
+    libraryDisplay()
+}
 
-function removeBook() {}
+// functions control whether form inputs are required
+
+function addRequiredField(element) {
+    element.classList.add('requiredField')
+}
+
+function removeRequiredField(element) {
+    element.classList.remove('requiredField')
+}
+
+function clearAllRequiredFields() {
+    const bookTitle = document.getElementById('titleInput')
+    const bookAuthor = document.getElementById('authorInput')
+    const bookPages = document.getElementById('pagesInput')
+    removeRequiredField(bookTitle)
+    removeRequiredField(bookAuthor)
+    removeRequiredField(bookPages)
+}
+
+function openForm() {
+    document.querySelector('form-container').style.display = 'block'
+    document.querySelector('formPopup').reset()
+    clearAllRequiredFields()
+}
 
 function closeForm() {
     document.getElementById('formContainer').style.display = 'none'
 }
+
+const btnNewBook = document.getElementById('bookBTN')
+btnNewBook.addEventListener('click', openForm)
+
 // book form functionality
 
 const addBook = document.getElementById('btnAddBook')
@@ -85,5 +123,28 @@ addBook.addEventListener('click', function(e) {
     const bookAuthor = document.getElementById('AuthorInput')
     const bookPages = document.getElementById('pagesInput')
     const bookRead = document.getElementById('hasRead')
-    if(!bookTitle)
+    if(!bookTitle.checkValidity()) {
+        addRequiredField(bookTitle)
+    } else if(bookTitle.checkValidity()) {
+        removeRequiredField(bookTitle)
+    }
+    if(!bookAuthor.checkValidity()) {
+        addRequiredField(bookAuthor)
+    } else if(bookAuthor.checkValidity()) {
+        removeRequiredField(bookAuthor)
+    }
+    if(!bookPages.checkValidity()) {
+        addRequiredField(bookPages)
+    } else if(bookPages.checkValidity()) {
+        removeRequiredField(bookPages)
+    }
+    if(bookTitle.checkValidity() && bookAuthor.checkValidity() && bookPages.checkValidity()) {
+        clearAllRequiredFields()
+        const bookN = new bookMaker(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.checked)
+        addBook(bookN)
+        libraryDisplay()
+        closeForm()
+    }
 })
+// if chain to determine if each form field recieved an input before sending
+// to the book constructor and adding the output to the library
